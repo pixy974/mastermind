64be3ea26c487e2fa93647ece8e00bb7cc651989
TURPIN Claire <c.turpin@ymail.com>
2021-10-26 16:56:36 +0200

    WOUAHOU Co-authored-by: kevingrand1 <kevingrand1@users.noreply.github.com>

--- src/App.java
+++ src/App.java
@@ -1,11 +1,37 @@
+import Utils.ConsoleColor;
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class App
 {
     // Cette méthode est appelée automatiquement au lancement de l'application
     public static void main(String[] args) throws Exception {
         // Efface la console
-        System.out.print("\033[H\033[2J");   
+        System.out.print("\033[H\033[2J");
         System.out.flush();
-        
-        System.out.print("Hello, world!");
+
+        //System.out.print(ConsoleColor.BLACK);
+        //System.out.print("Hello, world!"+ ConsoleColor.BLACK);
+        //System.out.print(ConsoleColor.RESET);
+        Mastermind mast = new Mastermind();
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println(mast.getKeyColor());
+
+        while (!mast.win()) {
+            System.out.println("Entrez une combinaison de couleur :");
+            String choice = scanner.nextLine();
+            System.out.println("Vous avez tapé: "+choice);
+            String userResponse = choice;
+            while (!userResponse.matches("^[RGYBMC]{4}$")) {
+                System.out.println("Les valeurs saisies ne sont pas valides, veuillez recommencer: ");
+                userResponse = scanner.nextLine();
+            }
+            System.out.println("résultat: "+mast.testCombination(choice));
+
+        }
+
+        scanner.close();
+
+
     }
 }
---
+++ src/Mastermind.java
@@ -0,0 +1,114 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Mastermind {
+    private String[] colors = {"R", "G", "Y", "B", "M", "C"};
+    private String[] keyColor;
+    private String answer;
+    Mastermind() {
+        //int ind;
+        keyColor = new String[4];
+        //générer la combinaison
+        //je choisis une couleur au hasard 4 fois dans le tableau colors et je rentre dans keyColor
+        for(int n = 0; n <= 3 ; n++)
+        {
+            //ind = (int) (Math.random()*6);
+            //this.keyColor[0] = 1;
+            // tant que le random renvoie une valeur déjà présente dans le tableau key color, je regénère un random
+
+            String rand = this.getRandomIndex();
+            int charaIndex = Arrays.asList(this.keyColor).indexOf(rand);
+            while(charaIndex >= 0){
+
+                rand = this.getRandomIndex();
+                charaIndex = Arrays.asList(this.keyColor).indexOf(rand);
+            }
+            this.keyColor[n] = rand;
+        }
+        System.out.println(""+ Arrays.toString(this.keyColor));
+    }
+
+    public String getKeyColor()
+    {
+        return this.keyColor.toString();
+    }
+
+    // Permet de generer une clé aléatoire à partir du tableau colors
+    public String getRandomIndex()
+    {
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(colors.length);
+        return colors[randomIndex];
+    }
+
+    public String testCombination(String comb)
+    {
+        // Comparer la combinaison de l'ordinateur a celle du joueur char par char
+        // S'il y a une corréspondance, on enleve du tableau du joueur
+        String[] combChar = comb.split("");
+
+        //va renvoyer O O - -
+        this.answer = "";
+
+        System.out.println(Arrays.toString(combChar));
+
+        for(int i = 0; i<this.keyColor.length; i++)
+        {
+            int charaIndex = Arrays.asList(combChar).indexOf(this.keyColor[i]);
+            if (charaIndex >= 0 && i == charaIndex) {
+                //combChar = this.removeTheElement(combChar, charaIndex);
+                this.answer += "O ";
+            } else if (charaIndex >= 0) {
+                this.answer += "X ";
+            } else {
+                this.answer += "- ";
+            }
+            System.out.println("position de "+(String)this.keyColor[i]+" dans "+Arrays.toString(combChar)+" = "+charaIndex);
+        }
+        return answer;
+    }
+
+    // Function to remove the element
+    public String[] removeTheElement(String[] arr, int index)
+    {
+
+        // If the array is empty
+        // or the index is not in array range
+        // return the original array
+        if (arr == null || index < 0
+            || index >= arr.length) {
+
+            return arr;
+        }
+
+        // Create another array of size one less
+        String[] anotherArray = new String[arr.length - 1];
+
+        // Copy the elements except the index
+        // from original array to the other array
+        for (int i = 0, k = 0; i < arr.length; i++) {
+
+            // if the index is
+            // the removal element index
+            if (i == index) {
+                continue;
+            }
+
+            // if the index is not
+            // the removal element index
+            anotherArray[k++] = arr[i];
+        }
+
+        // return the resultant array
+        return anotherArray;
+    }
+
+    public boolean win() {
+        if (this.answer == "O O O O " ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
